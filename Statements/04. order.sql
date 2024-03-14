a02dc67d8fb45fac9d293102094c41d83c874077
Hector Capera <109114807+HectorCapera@users.noreply.github.com>
2024-03-14 18:38:21 -0500

    First class

---
+++ Statements/04. order.sql
@@ -0,0 +1,9 @@
+SELECT * FROM users ORDER BY age; 
+
+SELECT * FROM users ORDER BY age  DESC; 
+
+SELECT * FROM users ORDER BY age  ASC; 
+
+SELECT name FROM users WHERE email= 'zahir@gmail.com' ORDER BY age DESC;
+
+SELECT * FROM users WHERE email= 'zahir@gmail.com' ORDER BY age DESC;
